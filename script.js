d63ae417ae7040b28dce1971084cabe4080a7a3f
hckf <leonc@live.co.uk>
2021-12-06 06:36:53 +0000

    Added FAQ section

---
+++ script.js
@@ -0,0 +1,13 @@
+let collapse = document.getElementsByClassName("faq-question");
+
+for (let i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
